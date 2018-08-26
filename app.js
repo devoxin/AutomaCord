a62f8d32dff52c96a0df6417dae94bbd429ced16
Devoxin <lukel9914@gmail.com>
2018-08-26 19:32:51 +0100

    bot stuff

--- app.js
+++ app.js
@@ -1,3 +1,31 @@
 const { Client } = require('eris');
-const webServer = require('./server');
+const config = require('./config');
 
+class Automa extends Client {
+  constructor(token, clientOptions) {
+    super(token, clientOptions);
+
+    this.webServer = require('./server');
+  }
+
+  start () {
+    this.webServer.start();
+    client.connect();
+  }
+}
+
+const bot = new Automa(config.bot.token);
+
+bot.on('messageCreate', (msg) => {
+  if (msg.author.bot || !msg.content.startsWith(config.bot.prefix)) {
+    return;
+  }
+
+  const [command, ...args] = msg.content.slice(config.bot.prefix).split(' ');
+
+  if (command === 'ping') {
+    msg.channel.createMessage('ponk :ping_pong:');
+  }
+});
+
+bot.start();
