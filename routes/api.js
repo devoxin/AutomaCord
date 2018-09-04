1d8fa87f0ce932b77063a16bd7c3524cd72442a4
Devoxin <lukel9914@gmail.com>
2018-09-04 18:29:36 +0100

    Basic API stuff

---
+++ routes/api.js
@@ -0,0 +1,37 @@
+const config = require('../config');
+const db = require('../utils/db');
+const express = require('express');
+
+class Route {
+  static async ensureBotExists (req, res, next) {
+    const { id } = req.params;
+
+    if (!id) {
+      return res.status(400).json({ message: 'Invalid Bot ID' });
+    }
+
+    const bot = await db.table('bots').get(id);
+
+    if (!bot) {
+      return res.status(404).json({ message: 'No bots found with that ID' });
+    }
+
+    req.bot = bot;
+    next();
+  }
+
+  static configure (server, bot) {
+    const router = express.Router();
+    server.use('/api', router);
+
+    router.get('/', (req, res) => {
+      res.render('error', { 'error': 'Docs pending.' });
+    })
+
+    router.get('/bot/:id', this.ensureBotExists, (req, res) => {
+      res.json(req.bot);
+    });
+  }
+}
+
+module.exports = Route;
